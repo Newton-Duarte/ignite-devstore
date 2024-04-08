847fd65d74889a57a3f1023ebf7596ba52982fa1
Newton Duarte <newton_@hotmail.com>
2024-04-08 10:23:51 -0300

    feat: add cart context provider to app store layout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
-import { Header } from '@/components/header'
 import { PropsWithChildren } from 'react'
+import { Header } from '@/components/header'
+import { CartContextProvider } from '@/contexts/cart-context'
 
 export default function StoreLayout({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
-      <Header />
-      {children}
-    </div>
+    <CartContextProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartContextProvider>
   )
 }
